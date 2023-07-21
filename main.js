b631b0d8af559884360de1e60d67b956c87cc6ae
batool-ahmed <ba06180@st.habib.edu.pk>
2023-07-21 23:57:42 +0500

    displaying weather after every 6 hours

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 var app = angular.module("weatherApp", []);
+
 app.controller("mainController", function ($scope, $http) {
     $scope.location = 'Karachi';
+    $scope.weatherFiveDays = []; 
 
     $scope.getWeather = function () {
         const API_KEY = '4aa6e82d736f430436f7e2e558f8a23b';
         var apiCall = "https://api.openweathermap.org/data/2.5/forecast?q=" + $scope.location + "&appid=" + API_KEY + "&units=metric";
 
         $http.get(apiCall).then(function (response) {
-            $scope.weather = response.data;
-            console.log($scope.weather);
+            const weather = response.data.list;
+
+            // 5 days weather after every 6 hours
+            $scope.weatherFiveDays = [];
+            for (var i = 0; i < weather.length; i += 2) {
+                $scope.weatherFiveDays.push(weather[i]);
+            }
+            console.log($scope.weatherFiveDays);
+
         }).catch(function (error) {
             $scope.error = 'Enter correct city name';
             console.error(error);

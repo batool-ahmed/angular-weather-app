286583506106c50c1d9b1ff9ef6e6a93d964c8d1
batool-ahmed <ba06180@st.habib.edu.pk>
2023-07-21 20:26:23 +0500

    setting up api call

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 var app = angular.module("weatherApp", []);
-app.controller("mainController", function ($scope){
+app.controller("mainController", function ($scope, $http){
     $scope.location = 'Karachi'
+    
+    $scope.getWeather = function () {
+        const API_KEY = '4aa6e82d736f430436f7e2e558f8a23b'; 
+        var apiCall = "https://api.openweathermap.org/data/2.5/weather?q=" + $scope.location + "&appid=" + API_KEY + "&units=metric";
+
+        $http.get(apiCall).then(function (response) {
+            $scope.weather = response.data;
+            console.log($scope.weather)
+        }).catch(function (error) {
+            console.error(error);
+        });
+    };
 });

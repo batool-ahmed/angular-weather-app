a58528754fc524d0fc007fe11c731b69c05bf0e4
batool-ahmed <ba06180@st.habib.edu.pk>
2023-07-21 22:50:15 +0500

    altering api call to display hourly forecast over 5 days

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 var app = angular.module("weatherApp", []);
-app.controller("mainController", function ($scope, $http){
-    $scope.location = 'Karachi'
-    
+app.controller("mainController", function ($scope, $http) {
+    $scope.location = 'Karachi';
+
     $scope.getWeather = function () {
-        const API_KEY = '4aa6e82d736f430436f7e2e558f8a23b'; 
-        var apiCall = "https://api.openweathermap.org/data/2.5/weather?q=" + $scope.location + "&appid=" + API_KEY + "&units=metric";
+        const API_KEY = '4aa6e82d736f430436f7e2e558f8a23b';
+        var apiCall = "https://api.openweathermap.org/data/2.5/forecast?q=" + $scope.location + "&appid=" + API_KEY + "&units=metric";
 
         $http.get(apiCall).then(function (response) {
             $scope.weather = response.data;
-            console.log($scope.weather)
+            console.log($scope.weather);
         }).catch(function (error) {
-            $scope.error = 'Enter correct city name'
+            $scope.error = 'Enter correct city name';
             console.error(error);
         });
     };
